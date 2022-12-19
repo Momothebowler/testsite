55a13dbadc7031efeadd91997727c8e11759c331
Momothebowler <momothebowler@gmail.com>
2022-12-19 10:31:56 -0500

    Design Changes
    Minor design changes
    - Added a nav bar
    - Changed background color
    - Added css and js for nav bar in both apps
    - Change to covariance table over correlation

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,19 +3,24 @@ from django.shortcuts import render
 from .models import *
 
 # import pandas as pd
-from portfolio.functions.funcs import get_correlation
+from portfolio.functions.funcs import get_covar
 import json
+import pandas
 
 
 def portfolio(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
     # item = money.objects.all().values()
 
-    df = get_correlation(json.loads(open("tickers.json").read())["tickers"])
+    df = get_covar(json.loads(open("tickers.json").read())["tickers"])
     # df = get_correlation(tickers)
     mydict = {
         "df": df.to_html(
-            border=10, classes="table table-striped text-center", justify="center"
+            float_format=lambda x: "%10.2f" % x,
+            border=3,
+            classes="table table-striped text-center",
+            justify="center",
+            col_space="38.25px",
         )
     }
     return render(request, "port.html", context=mydict)
