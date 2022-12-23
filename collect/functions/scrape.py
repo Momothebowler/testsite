1331d9c6d05523d154dce7dfb7e90795598d274b
Momothebowler <momothebowler@gmail.com>
2022-12-23 10:30:03 -0500

    Helpme plz

--- collect/functions/scrape.py
+++ collect/functions/scrape.py
@@ -87,9 +87,9 @@ def evaulate(ticks):
     options.add_argument("--disable-infobars")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-extensions")
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--start-maximized")
-    # options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--remote-debugging-port=9222")
     driver = webdriver.Chrome(
         options=options, executable_path=ChromeDriverManager().install()
     )
