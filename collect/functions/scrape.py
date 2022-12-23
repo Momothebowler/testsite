ddab1b5800c4aa3aa40529608824a57d9b189d9b
Momothebowler <momothebowler@gmail.com>
2022-12-23 10:58:50 -0500

    Help?

--- collect/functions/scrape.py
+++ collect/functions/scrape.py
@@ -79,6 +79,7 @@ def evaulate(ticks):
     arr = ["".join(item) for item in arr.astype(str)]
 
     options = Options()
+    options.binary_location = "/root/.nix-profile/bin/google-chrome-stable"
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
