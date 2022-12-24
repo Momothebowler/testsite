3f7817929ef147a55b7bd2b1d33609480c550dd4
Momothebowler <momothebowler@gmail.com>
2022-12-24 15:41:14 -0500

    Update scrape.py

--- collect/functions/scrape.py
+++ collect/functions/scrape.py
@@ -33,19 +33,13 @@ def evaulate():
     driver.get("https://www.portfoliovisualizer.com/optimize-portfolio")
     if len(ticks) / 10 >= 1:
         for i in range(int(len(ticks) / 10)):
-            more = WebDriverWait(driver, 10).until(
+            more = WebDriverWait(driver, 2).until(
                 EC.presence_of_element_located(("link text", "More"))
             )
             driver.execute_script("arguments[0].click();", more)
-    elements = driver.find_elements(By.XPATH, "//input")
+    ticker_inputs = driver.find_elements(By.CSS_SELECTOR, ("[name^=symbol]"))
+    allocation_inputs = driver.find_elements(By.CSS_SELECTOR, ("[name^=allocation]"))
 
-    ticker_inputs = []
-    allocation_inputs = []
-    for x in elements:
-        if "symbol" in x.get_attribute("name"):
-            ticker_inputs.append(x)
-        if "allocation" in x.get_attribute("name"):
-            allocation_inputs.append(x)
     for x in range(len(ticks)):
         ticker_inputs[x].send_keys(ticks[x])
         allocation_inputs[x].send_keys(arr[x])
