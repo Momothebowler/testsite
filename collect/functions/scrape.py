0b1003f86c36d68ce057968ccd373d8581ee65a8
Momothebowler <momothebowler@gmail.com>
2022-12-25 21:24:04 -0500

    Selenium is no more!

--- collect/functions/scrape.py
+++ collect/functions/scrape.py
@@ -1,15 +1,10 @@
 import requests
 import lxml.html as lh
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import numpy as np
 import re
 
 import requests
-import bs4
 from lxml import html, etree
 import re
 import pandas as pd
