ab44e6839e142de7b74681c29f6563022bfb3806
Momothebowler <momothebowler@gmail.com>
2023-01-17 15:17:10 -0500

    Bug Fix

--- collect/functions/funcs.py
+++ collect/functions/funcs.py
@@ -4,6 +4,7 @@ import datetime
 import re
 import numpy as np
 import random
+import traceback
 
 
 def get_data(
@@ -49,93 +50,109 @@ def get_data(
     )
 
     tree = html.fromstring(page.content)
-    message = ""
 
-    # gets any update message (might need a sooner time period)
-    try:
-        message = tree.xpath("/html/body/div[1]/div[3]")
-        message = etree.tostring(message[0])
-    except:
-        pass
+    # /html/body/div[1]/div[2]
+    # /html/body/div[1]/div[3]
+    # /html/body/div[1]/div[2]
 
     # Checks for the big bad error first
     try:
-        message4 = tree.xpath("/html/body/div[1]/div[2]")  # looks for an error message
-        message4 = etree.tostring(message4[0])
-        message5 = re.findall(
-            "is (.*?)<br/>",
-            message4.decode("utf-8"),
-        )
-        if message5 != []:
-            message5 = message5[0].split(" - ")[0].split(" ")
-            earliest_year = int(message5[1])
-            earliest_month = int(datetime.datetime.strptime(message5[0], "%b").month)
-            return True, [], {}, earliest_year, earliest_month, all_time_dict
+        bad_message = tree.xpath(
+            "/html/body/div[1]/div[2]"
+        )  # looks for an error message
+        bad_message = etree.tostring(bad_message[0]).decode()
+        if "alert alert-danger" in bad_message:
+            try:
+                # could make a function below
+                bad_message = re.findall("is (.*?)<br/>", bad_message)
+                bad_message = bad_message[0].split(" - ")[0].split(" ")
+                earliest_year = int(bad_message[1])
+                earliest_month = int(
+                    datetime.datetime.strptime(bad_message[0], "%b").month
+                )
+                return True, [], {}, earliest_year, earliest_month, all_time_dict
+            except:
+                return True, [], {}, earliest_year, earliest_month, all_time_dict
     except Exception as e:
         print(e)
         pass
 
-    try:
-        message3 = tree.xpath("/html/body/div[1]/div[3]")  # looks for an error message
-        message3 = re.findall(
-            "<div class='alert alert-danger''>(.*?)</div>",
-            message.decode("utf-8"),
-        )
-        if message3 != []:
-            message3 = re.findall("</b>(.*?)\n", message.decode("utf-8"))
-            message3 = re.findall("\[(.*?) -", message[0])[0].split(" ")
-            earliest_year = int(message3[1]) + 1
-            earliest_month = int(datetime.datetime.strptime(message3[0], "%b").month)
-            return True, [], {}, earliest_year, earliest_month, all_time_dict
-    except Exception as e:
-        pass
+    # gets any update message (might need a sooner time period)
+    # try:
+    #    note_message = tree.xpath("/html/body/div[1]")
+    #    note_message = etree.tostring(note_message[0]).decode()
+
+    # /html/body/div[1]/div[3]/text()
+    # print(note_message)
+    #    if (
+    #        '<div class="alert alert-primary alert-dismissible" role="alert">'
+    #        in note_message
+    #    ):
+    # print(note_message)
+    # could make a function below
+    #        note_message = re.findall(
+    #            '<div class="alert alert-primary alert-dismissible" role="alert">(.*?)</div>',
+    #            note_message,
+    #        )
+    #        note_message = re.findall("\[(.*?) -", note_message[0])[0].split(" ")
+    #        earliest_year = int(note_message[1]) + 1
+    #        earliest_month = int(
+    #           datetime.datetime.strptime(note_message[0], "%b").month
+    #        )
+    #        print("hi")
+    #        return True, [], {}, earliest_year, earliest_month, all_time_dict
+    # except Exception as e:
+    #    print(note_message)
+    #    pass
 
     # gets the maximum sharpe ratio table
-    trs = tree.xpath("//*[@id='growthChart']/div[2]/div[2]/div/div[1]/table")
-    trss = tree.xpath("//*[@id='growthChart']/div[3]")
+    portfolio_tree = tree.xpath("//*[@id='growthChart']/div[2]/div[2]/div/div[1]/table")
+    summary_tree = tree.xpath("//*[@id='growthChart']/div[3]")
 
     try:
         # Parses table data into tickers and %'s
-        e = etree.tostring(trs[0])
-        s = etree.tostring(trss[0])
+        sharpe_port_from_page = etree.tostring(portfolio_tree[0])
+        summary_from_page = etree.tostring(summary_tree[0])
 
-        value = re.findall('<td class="numberCell">(.*?)</td>', str(s))
+        summary = re.findall(
+            '<td class="numberCell">(.*?)</td>', str(summary_from_page)
+        )
 
-        ticker = re.findall("<td>(.*?)</td>", str(e))
+        ticker = re.findall("<td>(.*?)</td>", str(sharpe_port_from_page))
         tickers = [ticker[i] for i in range(len(ticker)) if i % 2 == 0]
 
-        percent = re.findall('<td class="numberCell">(.*?)</td>', str(e))
+        percent = re.findall(
+            '<td class="numberCell">(.*?)</td>', str(sharpe_port_from_page)
+        )
 
-        for y in range(len(posted_tickers)):
+        for ticker in posted_tickers:
             # checks if its in the dictionary yet and then if it was on the webpage
             # last part is if we a tick with 0% and another with 100% we need
             # to a 0 into the arr for averaging purposes
-            if posted_tickers[y] not in data_dict2:
+            if ticker not in data_dict2:
                 # Initial add of each ticker (first time scraped)
-                if posted_tickers[y] not in tickers:
+                if ticker not in tickers:
 
-                    received_data_dict[posted_tickers[y]] = [0]
+                    received_data_dict[ticker] = [0]
                 else:
-                    received_data_dict[posted_tickers[y]] = [
-                        percent[tickers.index(posted_tickers[y])]
-                    ]
+                    received_data_dict[ticker] = [percent[tickers.index(ticker)]]
             else:
-                if posted_tickers[y] not in tickers:
-                    art = data_dict2[posted_tickers[y]]
-                    art.append(0)
-                    received_data_dict[posted_tickers[y]] = art
+                if ticker not in tickers:
+                    temp_data_dict = data_dict2[ticker]
+                    temp_data_dict.append(0)
+                    received_data_dict[ticker] = temp_data_dict
                 else:
-                    art = data_dict2[posted_tickers[y]]
-                    art.append(percent[tickers.index(posted_tickers[y])])
-                    received_data_dict[posted_tickers[y]] = art
-        for pp in value:
+                    temp_data_dict = data_dict2[ticker]
+                    temp_data_dict.append(percent[tickers.index(ticker)])
+                    received_data_dict[ticker] = temp_data_dict
+        for pp in summary:
             try:
                 ppp = pp.split("/> ")
                 _summary_data.append(ppp[1])
             except:
                 _summary_data.append(pp)
     except Exception as ex:
-        print(ex)
+        traceback.print_exc()
         print(send_data)
         return True, [], {}, earliest_year, earliest_month, all_time_dict
     return (
