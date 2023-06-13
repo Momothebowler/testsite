474c5df310d46305610111d7fec0eb82ff573c3c
Momothebowler <momothebowler@gmail.com>
2023-06-13 16:38:36 -0400

    Adding Tooltips

--- collect/static/collect_table.js
+++ collect/static/collect_table.js
@@ -1,48 +1,54 @@
 var width = 1
 $('#post-form').submit(function (e) {
-    e.preventDefault();
-    document.getElementById("table-btn").style.display = "none";
-    document.getElementById("myProgress").style.display = "block";
-    data = {};
-    data['iters'] = $('#Iterations').val();
-    max = data['iters'];
-    data['csrfmiddlewaretoken'] = $('[name=csrfmiddlewaretoken]').val();
-    var count1 = document.querySelector('form .step-1').querySelector(".inp-group").childElementCount;
-    data['count1'] = count1
-    for (let x = 1; x <= count1; x++) {
-        data['symbol'.concat(parseInt(x))] = $('#symbol'.concat(parseInt(x))).val();
-        data['Allocation'.concat(parseInt(x)).concat("_1")] = $('#Allocation'.concat(parseInt(x)).concat("_1")).val();
+  e.preventDefault();
+  document.getElementById("table-btn").style.display = "none";
+  document.getElementById("myProgress").style.display = "block";
+  data = {};
+  data['iters'] = $('#Iterations').val();
+  max = data['iters'];
+  data['csrfmiddlewaretoken'] = $('[name=csrfmiddlewaretoken]').val();
+  var count1 = document.querySelector('form .step-1').querySelector(".inp-group").childElementCount;
+  data['count1'] = count1
+  for (let x = 1; x <= count1; x++) {
+    data['symbol'.concat(parseInt(x))] = $('#symbol'.concat(parseInt(x))).val();
+    data['Allocation'.concat(parseInt(x)).concat("_1")] = $('#Allocation'.concat(parseInt(x)).concat("_1")).val();
+  }
+  $.ajax({
+    type: 'POST',
+    url: 'create',
+    data: data,
+    success: function (data) {
+      var data_obj = JSON.parse(data);
+      $("div[name='Table1']").html("");
+      $("div[name='Table1']").html(data_obj.df);
+      $("div[name='Table2']").html("");
+      $("div[name='Table2']").html(data_obj.df2);
+      document.getElementById("table-btn").style.display = "";
+      document.getElementById("myProgress").style.display = "none";
+      width = 101;
+      for (let i = 1; i <= 2; i++) {
+        var innerText = document.querySelector("#Table".concat(i).concat("> table > tbody > tr:nth-child(8) > td:nth-child(1)")).innerHTML;
+        document.querySelector("#Table".concat(i).concat("> table > tbody > tr:nth-child(8) > td:nth-child(1)")).innerHTML = "<div class='tooltip'>".concat(innerText).concat("<span class='tooltiptext'>Most Money Made In A Single Year.</span></div>");
+      }
+
     }
-    $.ajax({
-        type: 'POST',
-        url: 'create',
-        data: data,
-        success: function (data) {
-            var data_obj = JSON.parse(data)
-            $("div[name='Table1']").html("");
-            $("div[name='Table1']").html(data_obj.df);
-            $("div[name='Table2']").html("");
-            $("div[name='Table2']").html(data_obj.df2);
-            document.getElementById("table-btn").style.display = "";
-            document.getElementById("myProgress").style.display = "none";
-            width = 101;   
-        }
-    });
+  });
 });
 
 
 function move() {
-  var elem = document.getElementById("myBar");   
+  var elem = document.getElementById("myBar");
   width = 1;
   var max = $('#Iterations').val();
   var id = setInterval(frame, max * 10);
+
   function frame() {
     if (width >= 100) {
       clearInterval(id);
       elem.style.width = 0 + '%';
     } else {
-      width++; 
-      elem.style.width = width + '%'; 
+      width++;
+      elem.style.width = width + '%';
     }
   }
 }
